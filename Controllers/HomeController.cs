9f670f5417dc1969036b0005c5a06851133d8a78
Luanha2003 <Luanha2003@users.noreply.github.com>
2025-08-13 18:18:38 +0700

    1231

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,31 +1,51 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using cuc.Models;
+using cuc.Models; // Thay "cuc" bằng tên dự án của bạn
 
-namespace cuc.Controllers;
-
-public class HomeController : Controller
+namespace cuc.Controllers // Thay "cuc" bằng tên dự án của bạn
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // === CÁC TRANG MỚI CHO WEB BÁN VÉ ===
+        public IActionResult Booking()
+        {
+            return View();
+        }
+
+        public IActionResult Routes()
+        {
+            // Trong tương lai bạn có thể tạo view "Routes.cshtml" để hiển thị lộ trình
+            return View();
+        }
+
+        public IActionResult TicketLookup()
+        {
+            // Trong tương lai bạn có thể tạo view "TicketLookup.cshtml" để tra cứu vé
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
